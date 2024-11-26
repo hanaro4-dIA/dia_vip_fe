8c1f8df97c6f799c489cabbb5d22907329b6c5b3
jangdayeon <dayeonjang.dev@gmail.com>
2024-11-26 09:50:30 +0900

    [style] : 오타 수정

--- components/RecommendCard.tsx
+++ components/RecommendCard.tsx
@@ -37,7 +37,7 @@ export default function RecommendCard() {
   return (
     <div className='bg-white shadow-lg rounded-lg'>
       <div className='border-b border-opacity-55 px-6 py-4'>
-        <div className='text-slate-600 text-2xl font-semibold'>맞춤 컨텐츠</div>
+        <div className='text-slate-600 text-2xl font-semibold'>맞춤 콘텐츠</div>
       </div>
       <div className='p-6'>
         <Swiper
